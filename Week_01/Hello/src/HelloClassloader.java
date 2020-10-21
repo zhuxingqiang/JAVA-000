8e7d517bfb6094e218b4e8cb287efc6b0094af95
xingqiang_zhu <xingqiang_zhu@163.com>
2020-10-21 23:41:25 +0800

    week01 作业一提交

---
+++ Week_01/Hello/src/HelloClassloader.java
@@ -0,0 +1,33 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Base64;
+
+/**
+ * @author xingqiang_zhu@163.com
+ * @date 2020/10/21
+ */
+public class HelloClassloader extends ClassLoader {
+
+
+    public static void main(String[] args){
+        String file = "yv66vgAAADQAHAoABgAOCQAPABAIABEKABIAEwcAFAcAFQEABjxpbml0PgEAAygpVgEABENvZGUBAA9MaW5lTnVtYmVyVGFibGUBAAVoZWxsbwEAClNvdXJjZUZpbGUBAApIZWxsby5qYXZhDAAHAAgHABYMABcAGAEAE0hlbGxvLCBjbGFzc0xvYWRlciEHABkMABoAGwEABUhlbGxvAQAQamF2YS9sYW5nL09iamVjdAEAEGphdmEvbGFuZy9TeXN0ZW0BAANvdXQBABVMamF2YS9pby9QcmludFN0cmVhbTsBABNqYXZhL2lvL1ByaW50U3RyZWFtAQAHcHJpbnRsbgEAFShMamF2YS9sYW5nL1N0cmluZzspVgAhAAUABgAAAAAAAgABAAcACAABAAkAAAAdAAEAAQAAAAUqtwABsQAAAAEACgAAAAYAAQAAAAEAAQALAAgAAQAJAAAAJQACAAEAAAAJsgACEgO2AASxAAAAAQAKAAAACgACAAAABAAIAAUAAQAMAAAAAgAN";
+        byte[] helloByte = Base64.getDecoder().decode(file);
+        Class hello = new HelloClassloader().defineClass("Hello", helloByte, 0, helloByte.length);
+        Object helloInstance;
+        Method method;
+        try {
+            helloInstance = hello.newInstance();
+            method = hello.getDeclaredMethod("hello");
+            method.invoke(helloInstance);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
